3aade6df8fe0c627a79e51010570cc317dd81842
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-07 15:56:53 -0700

    set up passport authentication

---
+++ config/auth.js
@@ -0,0 +1,23 @@
+//.config/auth.js
+var passport      = require('passport');
+var BasicStrategy = require('passport-http').BasicStrategy;
+var User          = require('../models/user.js');
+
+
+passport.use(new BasicStrategy(
+  function(username, password, next) {
+    User.findOne({username: username}, function(err, username){
+      if(err) return next(err);
+      if(!user) return next(null,false);
+
+      user.varifyPassword(password, function(err, isMatch){
+        if (err) return next(err);
+        if (!isMatch) return next(null, false);
+        return next(null, user);
+      });//End password verify
+    });//End findOne
+  }//End BasicStrategy
+));//End passport use
+
+
+exports.isAuthenticated = passport.authenticate('basic', { session : false });
