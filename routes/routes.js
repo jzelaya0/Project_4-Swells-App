46a431a6bcf09074c4f2187d41b5076b4e745b97
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-07 17:31:48 -0700

    routed controllers and models

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,35 @@
 var express = require('express');
 var Router  = express.Router();
 
+//CONTROLLERS
+// ================================================================
+authController         = require('../config/auth.js');
+usersController        = require('../controllers/users.js');
+surfSessionsController = require('../controllers/surf_sessions.js');
+
+//Authenticate Routes
+// ====================================
+Router.route('/authenticate')
+  .post(usersController.authenticate);
 
 
-Router.get('/', function(req,res){
-  res.json({message:"Hello"})
-})
+//User Routes
+// ====================================
+Router.route('/users')
+  .post(usersController.newUser)
+  .get(authController.isAuthenticated, usersController.getAllUsers);
+
+
+//Surf Sessions Routes
+// ====================================
+Router.route('/surf_sessions')
+  .get(surfSessionsController.getAll)
+  .post(authController.isAuthenticated, surfSessionsController.new);
+
+Router.route('/surf_sessions/:surf_sessions_id')
+  .get(authController.isAuthenticated, surfSessionsController.getOne)
+  .put(authController.isAuthenticated, surfSessionsController.edit)
+  .delete(authController.isAuthenticated, surfSessionsController.delete);
 
 
 module.exports = Router;
