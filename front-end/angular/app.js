f2a88e230c1b7691fddeb86649442fd25e24ce9c
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-08 17:27:04 -0700

    fixed syntax errors

--- front-end/angular/app.js
+++ front-end/angular/app.js
@@ -6,19 +6,19 @@
         'ngCookies'
     ])
     .run(runGlobals);
-
-    //Inject Dependencies
+    //
+    // //Inject Dependencies
     runGlobals.$inject = ['$http', '$rootScope', '$location', '$cookies'];
-
+    //
     function runGlobals($http, $rootScope, $location, $cookies){
       //Create object to hold globals
-      $rootScope.global = $cookies.get('globals') || {};
+      $rootScope.globals = $cookies.get('globals') || {};
 
       //Set auth data for set users
       if($rootScope.globals.currentUser) {
          $http.defaults.headers.common['Authorization'] = 'Basic ' + $rootScope.globals.currentUser.authdata;
       }
-
+      //
       //Check to see if user is logged in
       $rootScope.$on('$locationChangeStart', function(evt, next, current){
         //Redirect if user is not logged in
