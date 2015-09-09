3e108590f15f4380dd4d20d23df22d1430920a5a
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-09 11:03:10 -0700

    restructured workflow

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var passport   = require('passport');
 var bodyParser = require('body-parser');
 var morgan     = require('morgan');
 var port       = process.env.PORT || 3000;
+var path       = require('path');
 
 
 //CONNTECT TO DATABASE
@@ -20,18 +21,39 @@ db.once('open', function(data){
   console.log("Succesfull database connection");
 })
 
+
+
 //EXPRESS CONFIGURATION
 // ==============================
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
+
 app.use(morgan('dev'));//Log all requests to the console
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());// Grab information from html forms
 
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+	next();
+});
+
 //Use passport in app
 app.use(passport.initialize());
 
-//Require Routes.js
+//Require Api Routes.js
 routes = require('./routes/routes.js');//load routes
 app.use('/api', routes);
+
+// MAIN CATCHALL ROUTE ---------------
+// SEND USERS TO FRONTEND ------------
+// has to be registered after API ROUTES
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
 
 
 //LAUNCH PORT
