4437d674081e3638969e5a28de31d42d70024e03
Jesse Zelaya <jessezelaya1@gmail.com>
2015-09-09 19:16:33 -0700

    altered models

--- models/user.js
+++ models/user.js
@@ -1,43 +1,40 @@
 //./models/user.js
 var mongoose  = require('mongoose');
 var Schema    = mongoose.Schema;
-var bcrypt    = require('bcrypt');
+var bcrypt 		= require('bcrypt-nodejs');
 
 //UserSchema
 // ==============================
-var userSchema = new Schema({
-  username: String,
-  email: {type: String, requried: true, unique: true},
-  password: {type: String, required: true, select: false}
+var UserSchema = new Schema({
+	name: String,
+	username: { type: String, required: true, index: { unique: true }},
+	password: { type: String, required: true, select: false }
 });
 
-//Hash user password before saving
-userSchema.pre('save',function(next){
-  var user = this;
+// Hash before saving
+UserSchema.pre('save', function(next) {
+	var user = this;
+  // hash the password for new users or changes
+	if (!user.isModified('password')) return next();
 
-  //Hash password for new or changed passwords
-  if(!user.isModified('password')) return next();
+	// Hash
+	bcrypt.hash(user.password, null, null, function(err, hash) {
+		if (err) return next(err);
 
-  //Generate Salt
-  bcrypt.genSalt(5, function(err,salt){
-      if(err) return next(err);
-    bcrypt.hash(user.password, salt, function(err,hash){
-      if(err) return next(err);
-      //Change the password to the hashed version
-      user.password = hash;
-      next();
-    });//end hash
-  });//end generate salt
-});//end save
+		// change the password to the hashed version
+		user.password = hash;
+		next();
+	});
+});
 
 //Method to compare given password with the database hash
-userSchema.methods.authenticate = function(password){
-  var user = this;
-  return bcrypt.compareSync(password, user.password);
-}
+UserSchema.methods.comparePassword = function(password) {
+	var user = this;
+	return bcrypt.compareSync(password, user.password);
+};
 
 
-//Create User model out of userSchema
-var User = mongoose.model('User', userSchema);
+//Create User model out of UserSchema
+var User = mongoose.model('User', UserSchema);
 
 module.exports = User;
